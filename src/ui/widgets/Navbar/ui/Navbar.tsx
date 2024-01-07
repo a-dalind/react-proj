00ace89b9179c5b7e61e7d56d22c7e3d45b11839
Diana Berdenko <cutie.cutie.di@gmail.com>
2024-01-07 23:05:07 +0300

    fix node versions

--- src/ui/widgets/Navbar/ui/Navbar.tsx
+++ src/ui/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,8 @@
 import { classNames } from 'lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'ui/components/AppLink/AppLink';
+// import { AppLink, AppLinkTheme } from 'ui/components/AppLink/AppLink';
 import { ThemeSwitcher } from 'ui/widgets/ThemeSwitcher';
 import { LangSwitcher } from 'ui/widgets/LangSwitcher';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,21 +10,21 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-    const { t } = useTranslation();
+    // const { t } = useTranslation();
 
     return (
         <div
             className={classNames(styles.navbar, {}, [className])}
             data-testid="navbar"
         >
-            <div className={styles.links}>
-                <AppLink theme={AppLinkTheme.NAVBAR} to="/" className={styles.link}>
-                    {t('Главная')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.NAVBAR} to="/about" className={styles.link}>
-                    {t('О сайте')}
-                </AppLink>
-            </div>
+            {/*<div className={styles.links}>*/}
+            {/*    <AppLink theme={AppLinkTheme.NAVBAR} to="/" className={styles.link}>*/}
+            {/*        {t('Главная')}*/}
+            {/*    </AppLink>*/}
+            {/*    <AppLink theme={AppLinkTheme.NAVBAR} to="/about" className={styles.link}>*/}
+            {/*        {t('О сайте')}*/}
+            {/*    </AppLink>*/}
+            {/*</div>*/}
 
             <div className={styles.switchers}>
                 <LangSwitcher />
